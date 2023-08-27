2fae2c191bb18cdd8e43e04fd830a0dd425ccb57
MVPD <mvpdiaz48@gmail.com>
2023-08-27 16:58:49 -0300

    proyectonext

---
+++ src/pages/inicio-sesion.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+
+//como agrego comprobacion a email en formato?
+
+export default function Inicio_sesion() {
+    return(   
+        <> 
+            <form>
+                <h1>INICIAR SESIÓN</h1>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Dirección de Email</label> <br/>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </input>    <br/>
+                    <label for="exampleInputPassword1" class="form-label">Contraseña</label><br/>
+                    <input type="password" class="form-control" id="exampleInputPassword1" maxLength={8}>
+                    </input>
+                </div>
+                <button type="submit" class="btn btn-primary">ENVIAR</button>
+            </form>
+            <div>
+                <Link href="/registro">Registrate</Link>
+            </div>
+        </>
+    )
+
+}
+
+/*<>
+<h4>Sino sos usuario registrado:</h4>
+<Link to="/registro">REGISTRATE</Link>
+</>*/
---
+++ src/pages/registro.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
+import Link from 'next/link';
+
+const Registro = () => (
+  <>
+    <div>
+      <h1>REGISTRATE</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+      <Form>
+        <label htmlFor="firstName">NOMBRE</label> <br/>
+        <Field id="firstName" name="firstName" placeholder="Nombre" /><br/>
+        <label htmlFor="lastName">APELLIDO</label><br/>
+        <Field id="lastName" name="lastName" placeholder="Apellido" /><br/>
+        <label htmlFor="email">Email</label><br/>
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@gmail.com"
+          type="email"
+        />
+        <button type="submit">ENVIAR</button>
+      </Form>
+      </Formik>
+    </div>
+    <div>
+    <Link href="/inicio-sesion">Iniciar Sesión</Link>
+    </div>
+  </>
+);
+
+//ReactDOM.render(<Registro />,document.getElementById('root'));
+export default Registro;
+
+
